573a4bb635be55c1c07985a2eec9d515be7aa72b
Kadirhan Uçar <79535417+kdrhnucr@users.noreply.github.com>
2021-05-13 05:41:38 -0400

    Update python3-strong-password-generator.py

--- python3-strong-password-generator.py
+++ python3-strong-password-generator.py
@@ -4,4 +4,5 @@ character = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ01234567890!?"
 lenght = int(input("How many characters do you want it to be : "))
 password =  "".join(random.sample(character,lenght ))
 
-print (password)
+new_password = "Your new strong password is :" 
+print (new_password,password)
