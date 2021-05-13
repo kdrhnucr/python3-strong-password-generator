14b278f286de7412b5b11adaba8ee9a96deb087f
Kadirhan Uçar <79535417+kdrhnucr@users.noreply.github.com>
2021-05-13 05:42:47 -0400

    Update python3-strong-password-generator.py

--- python3-strong-password-generator.py
+++ python3-strong-password-generator.py
@@ -1,6 +1,6 @@
 import random
 
-character = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ01234567890!?"
+character = "abcdefghijklmnopqrstuvwxyz!?ABCDEFGHIJKLMNOPQRSTUVWXYZ01234567890"
 lenght = int(input("How many characters do you want it to be : "))
 password =  "".join(random.sample(character,lenght ))
 
